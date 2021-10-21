7bd6583ed0131e3700583b9cfc0023705e4e8300
EdwardBurgos <edwardpbn@gmail.com>
2021-10-21 07:56:44 -0500

    ssl database connection if not in development

--- src/db.js
+++ src/db.js
@@ -14,8 +14,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: process.env.DIALECTOPTIONS
-});
+  dialectOptions: process.env.NODE_ENV === 'development' ? null :
+    {
+      ssl: {
+        required: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 
 const basename = path.basename(__filename);
 
